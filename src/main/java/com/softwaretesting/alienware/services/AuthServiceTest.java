9a0b60f1e3f077f50d76fbc72a645c64c4a86a77
Manutsawin <manassawin369@gmail.com>
2023-02-18 16:00:29 +0700

    update

--- src/main/java/com/softwaretesting/alienware/services/AuthServiceTest.java
+++ src/main/java/com/softwaretesting/alienware/services/AuthServiceTest.java
@@ -5,7 +5,7 @@ public class AuthServiceTest {
     private AuthService authService;
 
     void should_return_success_when_call_plus_with_number1_and_number2(){
-        String result = authService.getUser();
-        assertEquals("Admin",result);
+        boolean result = authService.compare("Admin","Admin1234");
+        assertEquals(true,result);
     }
 }
